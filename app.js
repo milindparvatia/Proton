71ae0ca551350e6695694c4b9647fd00906f744e
Milind Parvatia <milind1997@gmail.com>
2018-01-15 13:13:01 +0530

    Express init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+// Index Route
+qpp.get();
+
+const port = 5000;
+
+app.listen(port,() =>{
+    console.log(`Server Strated on Port ${port}`);
+})
