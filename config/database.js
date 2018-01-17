9ccf1dd10ca09ebb29af4a877ab86fb54cd9d355
Milind Parvatia <milind1997@gmail.com>
2018-01-18 02:31:44 +0530

    finale commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://<milind>:<12@fuck@34>@ds259897.mlab.com:59897/vidjot-db'}
+  module.exports = {mongoURI: 'mongodb://<milindsp>:<12@fuck@34>@ds259897.mlab.com:59897/vidjot-db'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
