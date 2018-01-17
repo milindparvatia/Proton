43c35ae1ecb862c402a6c7d8d4156f00e16f8ff1
Milind Parvatia <milind1997@gmail.com>
2018-01-18 01:57:12 +0530

    work complete

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const express = require('express');
+const path = require('path');
 const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
 
-//Load routes
+// Load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
+// DB Config
+const db = require('./config/database');
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
 // Connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev', {
+mongoose.connect(db.mongoURI, {
   useMongoClient: true
 })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
-
-
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -33,6 +38,9 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Method override middleware
 app.use(methodOverride('_method'));
 
@@ -43,6 +51,10 @@ app.use(session({
   saveUninitialized: true
 }));
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 // Global variables
@@ -50,6 +62,7 @@ app.use(function(req, res, next){
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
@@ -66,11 +79,12 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-//Use Routes
-app.use('/ideas',ideas);
-app.use('/users',users);
 
-const port = 5000;
+// Use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
